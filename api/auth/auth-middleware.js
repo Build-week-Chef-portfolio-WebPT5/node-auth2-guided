2aaf196fa49af50ce7c29c6e7fe1c4ab1ee63a07
James Blacklock <jamesblacklock@protonmail.com>
2022-04-26 13:27:07 -0400

    committing to "lecture"

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -6,11 +6,6 @@ const restricted = (req, res, next) => {
   const token = req.headers.authorization;
   const MESSAGE_401 = 'You must be logged in to access this API';
 
-  if(token == null) {
-    next({ status: 401, message: MESSAGE_401 });
-    return;
-  }
-
   jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
     if(err) {
       next({ status: 401, message: MESSAGE_401 });
