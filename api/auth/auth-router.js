ac228aff54a1ef4bcf6e60320743e90a19ed8389
Fritz <fritz@icloud.com>
2022-05-09 14:33:55 -0500

    add jwt

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,37 +1,53 @@
-const bcrypt = require('bcryptjs')
+const bcrypt = require("bcryptjs");
+const router = require("express").Router();
+const jwt = require("jsonwebtoken");
 
-const router = require('express').Router()
-const User = require('../users/users-model.js')
+const User = require("../users/users-model.js");
 
-const { BCRYPT_ROUNDS } = require('../../config')
+const { BCRYPT_ROUNDS, JWTSECRET } = require("../../config");
 
-router.post('/register', (req, res, next) => {
-  let user = req.body
+router.post("/register", (req, res, next) => {
+  let user = req.body;
 
   // bcrypting the password before saving
-  const hash = bcrypt.hashSync(user.password, BCRYPT_ROUNDS)
+  const hash = bcrypt.hashSync(user.password, BCRYPT_ROUNDS);
   // never save the plain text password in the db
-  user.password = hash
+  user.password = hash;
 
   User.add(user)
-    .then(saved => {
-      res.status(201).json({ message: `Great to have you, ${saved.username}` })
+    .then((saved) => {
+      res.status(201).json({ message: `Great to have you, ${saved.username}` });
     })
-    .catch(next) // our custom err handling middleware in server.js will trap this
-})
+    .catch(next); // our custom err handling middleware in server.js will trap this
+});
 
-router.post('/login', (req, res, next) => {
-  let { username, password } = req.body
+router.post("/login", (req, res, next) => {
+  let { username, password } = req.body;
 
   User.findBy({ username })
     .then(([user]) => {
       if (user && bcrypt.compareSync(password, user.password)) {
-        res.status(200).json({ message: `Welcome back ${user.username}...` })
+        const token = generateToken(user);
+
+        res
+          .status(200)
+          .json({ message: `Welcome back ${user.username}...`, token });
       } else {
-        next({ status: 401, message: 'Invalid Credentials' })
+        next({ status: 401, message: "Invalid Credentials" });
       }
     })
-    .catch(next)
-})
+    .catch(next);
+});
 
-module.exports = router
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+  };
+  const options = {
+    expiresIn: "8hr",
+  };
+  return jwt.sign(payload, JWTSECRET, options);
+}
+
+module.exports = router;
--- config/index.js
+++ config/index.js
@@ -1,5 +1,8 @@
 module.exports = {
   BCRYPT_ROUNDS: process.env.BCRYPT_ROUNDS || 8,
-  NODE_ENV: process.env.NODE_ENV || 'development',
+  NODE_ENV: process.env.NODE_ENV || "development",
   PORT: process.env.PORT || 9000,
-}
+  JWTSECRET:
+    process.env.JWT_SECRET ||
+    "eyJzdWJqZWN0IjoyLCJ1c2VybmFtZ-SI6ImZyaXR6Z3QiLCJp-YXQiOjE2NTIxMjExNTA",
+};
